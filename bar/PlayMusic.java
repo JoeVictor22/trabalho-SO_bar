f37f94ef9b1a35beb5783a11f042a1b9f67cc57c
raynan222 <38507894+raynan222@users.noreply.github.com>
2020-03-20 12:09:39 -0300

    Update PlayMusic.java

--- bar/PlayMusic.java
+++ bar/PlayMusic.java
@@ -2,27 +2,26 @@ package bar;
 
 import javax.swing.JOptionPane;
 
-import sun.audio.AudioPlayer;
-
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
-import sun.audio.AudioPlayer;
-import sun.audio.AudioStream;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequencer;
+
 
 public class PlayMusic {
 
 
 	public static void playMusic(String filepath) {
-		InputStream music;
-		
 		try {
-			music = new FileInputStream(new File(filepath));
-			AudioStream audios = new AudioStream(music);
-			AudioPlayer.player.start(audios);
+			Sequencer sequencer = MidiSystem.getSequencer();
+			sequencer.open();
+			InputStream music = new BufferedInputStream(new FileInputStream(new File(filepath)));
+	        sequencer.setSequence(music);
+	        sequencer.start();
 		}catch(Exception e) {
 			JOptionPane.showMessageDialog(null, "Error");
 		}
 	}
 }
-
