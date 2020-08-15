3f4eb30acadbbd76cf261766e91696c6cc26712e
raynan222 <38507894+raynan222@users.noreply.github.com>
2020-08-15 15:59:16 -0300

    Update Popup.java
    finalizado

--- bar/Popup.java
+++ bar/Popup.java
@@ -1,35 +1,35 @@
 package bar;
 
-import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 public class Popup{
 	int numero;
-
+	String titulo="Papudim Simulator";
+	String informacao="Informa a quantidade de cadeiras";
+	
 	public Popup() {
-		super();
 		this.receber();
 	}
     
-    public int getNumero() {
-		return numero;
-	}
-
-	public void setNumero(int numero) {
-		this.numero = numero;
-	}
-
-	public void receber() 
+	public void receber()
 	{
 		while(true)
 		{
-			final JFrame parent = new JFrame();
-	        String cadeira = JOptionPane.showInputDialog(parent,
-	                "Informa a quantidade de cadeiras", null);
-	        
-	        this.setNumero(Integer.parseInt(cadeira));
-	        
-	        if(this.getNumero() > 0) break;
+		    String cadeira = JOptionPane.showInputDialog(null,
+	               informacao, titulo, JOptionPane.INFORMATION_MESSAGE);
+		    try {
+		    	this.setNumero(Integer.parseInt(cadeira));
+		    }catch (NumberFormatException nfe) {		        
+		    }
+		    if(this.getNumero() > 0 && this.getNumero() < 21) break;
+		    else if(cadeira==null)System.exit(0);
 		}
     }
+	
+    public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
 }
